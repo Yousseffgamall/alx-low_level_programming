789fc613ebe8bdebe2beece5d160c99a01436dbb
Youffgamall <yg0381388@gmail.com>
2023-07-13 22:18:08 +0000

    c hello world task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
  *description : a c program that prints with pus tfunction
  *
- * Return: Always 0 (success) 
+ * Return: lways 0 (success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
