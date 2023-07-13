962b1c0ea21b44d395de69f8cd8fba99b0518a22
Youffgamall <yg0381388@gmail.com>
2023-07-13 22:58:47 +0000

    c hello world task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point of the program
+  *
+  * Description: This program prints the size of various types
+  * on the computer it is compiled and run on.
+  *
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of double: %zu byte(s)\n", sizeof(double));
+	printf("Size of long: %zu byte(s)\n", sizeof(long));
+	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+
+	return (0);
+}
