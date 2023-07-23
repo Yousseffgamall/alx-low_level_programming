a24130b28a6b9891d83a08d6f5c021cf14e15425
Youffgamall <yg0381388@gmail.com>
2023-07-23 08:01:52 +0000

    c task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function serves as the entry point of the program.
+ * It executes the main functionality of the program.
+ *
+ * Return: 0 on success, non-zero value on error
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("the number is positive.\n", n);
+	} else if (n == 0)
+	{
+		printf("the number is zero.\n", n);
+	} else
+	{
+		printf("the number is negative.\n", n);
+	}
+
+	return (0);
+}
